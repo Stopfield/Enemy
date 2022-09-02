002ee630406953d66ab4bbd32d2320df9fb0e8cd
Stopfield <thiagoparacampo@hotmail.com>
2022-09-02 19:58:33 -0300

    Creating two instances of Enemy class

--- main.cpp
+++ main.cpp
@@ -6,9 +6,15 @@
 int main() {
 
     Enemy hogoblin("Hogoblin", 15, 20);
+    Enemy bokoblin("Bokoblin", 30, 15);
+
     hogoblin.printStatus();
     hogoblin.decreaseHP(5);
     hogoblin.printStatus();
+
+    bokoblin.printStatus();
+    bokoblin.decreaseHP(10);
+    bokoblin.printStatus();
 
     return 0;
 }
