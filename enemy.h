079ab0a73ea54813e5e989b673ec5a2e736b6844
Stopfield <thiagoparacampothig@gmail.com>
2022-09-09 17:03:49 -0300

    Good programming practice: public is always on top of private!

--- enemy.h
+++ enemy.h
@@ -5,17 +5,17 @@
 
 
 class Enemy {
-    private:
-        std::string name;
-        int healthPoints;
-        int defensePoints;
-    
     public:
         Enemy(std::string name, int healthPoints, int defensePoints);
         ~Enemy();
 
         void printStatus();
         void decreaseHP(int);
+
+    private:
+        std::string name;
+        int healthPoints;
+        int defensePoints;
 };
 
 #endif
